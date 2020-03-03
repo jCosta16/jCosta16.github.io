7d0ff7c7052e48801812f33f8237012d40c59f79
Joao Costa <costa.16@yahoo.com>
2020-03-02 19:27:02 -0500

    Update app.js

--- d3_data/assets/js/app.js
+++ d3_data/assets/js/app.js
@@ -21,7 +21,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../D3_data_journalism/assets/data/data.csv").then(function(healthData) {
+d3.csv("assets/data/data.csv").then(function(healthData) {
     var states = []
     // Step 1: Parse Data/Cast as numbers
     // ==============================
