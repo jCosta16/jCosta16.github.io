c67a7d75225daa1931ee2978a91cbaf24c83401b
Joao Costa <costa.16@yahoo.com>
2020-03-10 18:03:12 -0400

    Add files via upload

--- Basketball/static/js/data_select.js
+++ Basketball/static/js/data_select.js
@@ -1,6 +1,17 @@
 var year_data;
+// funtion to find multiple max values
+function multiplemax(test_array) { 
+  let arr = Object.values(test_array);
+  let max = Math.max(...arr)
+  let listtest =[]
+  for (let [key, value] of Object.entries(test_array)) {
+    if(value == max){
+      listtest.push(key)
+    }
+  }
+  return [listtest, max];
+};
 function buildMetadata(sample) {
-  // @TODO: Complete the following function that builds the metadata panel
   d3.json("./static/data/NBAdataJSON.json").then(function(nbaData) {
     var sorted_year = [];
     if (sample == "All Data"){
@@ -18,40 +29,38 @@ function buildMetadata(sample) {
     var maxSalary = _.max(sorted_year, function (player) {
       return player.salary;
     });
+    var salary_index = sorted_year.indexOf(maxSalary); 
+    var high_salary = sorted_year[salary_index];
     
     // most drafted position
     var position_drafted = _.countBy(sorted_year, function (player) {
       return player.position;
     });
+    var top_position = multiplemax(position_drafted);
+    
+    // most drafted college
     var college_drafted = _.countBy(sorted_year, function (player) {
       return player.college;
     });
-    var top_position = _.max(Object.keys(position_drafted), pos => position_drafted[pos]);
-    var top_college = _.max(Object.keys(college_drafted), col => college_drafted[col])
-
-    var salary_index = sorted_year.indexOf(maxSalary); 
-    var high_salary = sorted_year[salary_index];
+    var top_college = multiplemax(college_drafted);
 
     // Inserting metadata
     var sampleMeta = d3.select("#sample-metadata").html("");
     sampleMeta.append("p").text("Highest salary: ").attr("class","b")
     .append("span").text(`${high_salary.name}, ${high_salary.position} at ${high_salary.team}`).attr("class", "n");
-    sampleMeta.append("p").text("Top Position: ").attr("class","b")
-    .append("span").text(`${top_position}`).attr("class", "n");
-    sampleMeta.append("p").text("Top College: ").attr("class","b")
-    .append("span").text(`${top_college}`).attr("class", "n");
-
+    sampleMeta.append("p").text("Top Drafted Position: ").attr("class","b")
+    .append("span").text(`${top_position[0]}, ${top_position[1]}`).attr("class", "n");
+    sampleMeta.append("p").text("Top Drafted College: ").attr("class","b")
+    .append("span").text(`${top_college[0]}, ${top_college[1]}`).attr("class", "n");
+    
     // console.log(JSON.stringify(sorted_year));
     year_data = sorted_year;
-   // console.log(new_data)
-    // export {sorted_year}; // a list of exported variables
+
   });
 };
 
 
-
 function init() {
-
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
     // Use the list of sample names to populate the select options
@@ -65,16 +74,13 @@ function init() {
         }
     }
     year_list = (Array.from(new Set(year_list))).sort()
-
-
-    // for (var i=0; i < year_list.length; i++) {
-      year_list.reverse().forEach((year) => {
-        selector
-          .append("option")
-          .text(year)
-          .property("value", year);
-      });
-    // }
+   
+    year_list.reverse().forEach((year) => {
+      selector
+        .append("option")
+        .text(year)
+        .property("value", year);
+    });
     
     // Use the first sample from the list to build the initial plots
     const firstSample = year_list.reverse()[year_list.length -1];
@@ -87,11 +93,8 @@ function init() {
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
-
   buildMetadata(newSample);
   buildPosition(newSample);
-
-  
 };
 
 // Initialize the dashboard
